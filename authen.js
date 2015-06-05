59d1a6af023351891b16a1165918543d6fee6ff9
arklian <emeraldarrow@gmail.com>
2015-06-05 18:09:00 -0400

    node_modules

---
+++ authen.js
@@ -0,0 +1,43 @@
+var mysql = require('mysql');
+var mysqlconnection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'spam',
+    password : 'spam',
+    database : 'eventping'
+});
+mysqlconnection.connect();
+
+//Authenticate function to check if the user is in the database
+exports.authenicate = function(user,pass,callback){  
+    var auth = false;
+    var query = 'SELECT * from users where username = "'+user+'" and password = "'+pass+'"';
+
+    //Find all users with user/pass combination
+    mysqlconnection.query(query,function(err,rows,fields){
+	//If a user is found, set auth to true 
+	auth = (rows.length == 1);
+	callback(auth);
+    });
+};
+
+//Check if the user name exists in the database
+exports.existsuser = function(user,pass,callback){    
+    var auth = false;
+    var query = 'SELECT * from users where username = "'+user+'"';
+
+    //Find all users with the username
+    mysqlconnection.query(query,function(err,rows,fields){  
+	//If a user is found, set auth to true
+	auth = (rows.length == 1);
+	callback(auth);
+    });
+};
+
+//Add the user/pass combination to the database
+exports.adduser = function(user,pass,callback){
+    var query = 'INSERT INTO users (username,password,name) VALUES ("'+user+'","'+pass+'","tempname")';
+    
+    mysqlconnection.query(query,function(err,rows,fields){
+	callback();
+    });
+}
