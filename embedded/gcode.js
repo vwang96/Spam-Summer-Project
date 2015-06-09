968e9901ad1aade7bd5bcd307e49b7d3f458a4dd
haoxinLuo <luoh2@rpi.edu>
2015-06-09 14:54:16 -0400

    added ETA and directions to event from current location

--- embedded/gcode.js
+++ embedded/gcode.js
@@ -3,15 +3,16 @@
 // pick list containing a mix of places and predicted search terms.
 
 function nameSpace(){
-    var event_location;
-    var event_latlng;
-    var D_BOUNDS="";
+    var event_location,event_latlng,dirService,dirDisplay,D_BOUNDS,map;
     var markers = [];
     var eButton = document.getElementById("confirmEvent");
     
     function initialize() {
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
-				      {mapTypeId:google.maps.MapTypeId.ROADMAP});
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+			{mapTypeId:google.maps.MapTypeId.ROADMAP});
+	dirService = new google.maps.DirectionsService();
+	dirDisplay = new google.maps.DirectionsRenderer({
+	    suppressMarkers:true});
 	if(navigator.geolocation){
 	    navigator.geolocation.getCurrentPosition(function(position){
 		var lat = position.coords.latitude;
@@ -35,14 +36,13 @@ function nameSpace(){
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 	
 	var searchBox = new google.maps.places.SearchBox(
-	    /** @type {HTMLInputElement} */(input));                             
-	
+	    /** @type {HTMLInputElement} */(input)); 	
 	// [START region_getplaces]
 	// Listen for the event fired when the user selects an item from the
 	// pick list. Retrieve the matching places for that item.
 	google.maps.event.addListener(searchBox, 'places_changed', function() {
 	    var places = searchBox.getPlaces();
-	    
+	    dirDisplay.setMap(null);
 	    if (places.length == 0) {
 		return;
 	    }
@@ -94,12 +94,15 @@ function nameSpace(){
 	    var eLocation = document.getElementById("eventLoc");
 	    var latlng = new google.maps.LatLng(marker.getPosition().A,
 						marker.getPosition().F);
+	    dirDisplay.setMap(null);
 	    map.setZoom(16);
 	    map.setCenter(marker.getPosition());
 	    gCoder.geocode({'latLng': latlng}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
 		    eLocation.innerHTML = marker.title + " @ "+ 
-			results[0].formatted_address;	
+			results[0].formatted_address;
+		    event_location = eLocation.innerHTML;
+		    event_latlng = latlng;
 		    createEventObj(results[0].formatted_address,
 				   marker.title,map);	
 		}
@@ -119,10 +122,13 @@ function nameSpace(){
 	c1.innerHTML = name+" "+addr;
 	r.addEventListener("click",function(){
 	    var gCoder = new google.maps.Geocoder();
+	    dirDisplay.setMap(null);
 	    gCoder.geocode({"address":addr},function(results,status){
 		if(status == google.maps.GeocoderStatus.OK){
 		    var eLocation = document.getElementById("eventLoc");
 		    eLocation.innerHTML = name + " @ "+addr;
+		    event_location = eLocation.innerHTML;
+		    event_latlng = results[0].geometry.location;
 		    map.setCenter(results[0].geometry.location);
 		    var marker = new google.maps.Marker({
 			map: map,
@@ -140,9 +146,44 @@ function nameSpace(){
 	});
     }
     
-    eButton.addEventListener("click",function(){
-	console.log("clicked");
-    });
+    function calcETA(){
+	dirDisplay.setMap(map);
+	var lat,lng;
+	if(navigator.geolocation){
+	    navigator.geolocation.getCurrentPosition(function(position){
+		lat = position.coords.latitude;
+		lng = position.coords.longitude;
+		var request = {
+		    origin: new google.maps.LatLng(lat,lng),
+		    destination: event_latlng,
+		    travelMode: google.maps.TravelMode.DRIVING
+		};
+		dirService.route(request, function(response, status) {
+		    if (status == google.maps.DirectionsStatus.OK) {
+			var ETA = document.getElementById("ETA");
+			dirDisplay.setDirections(response);
+			ETA.innerHTML = "ETA: "+response.routes[0].legs[0].duration.text;
+			loadSteps(response.routes[0].legs[0].steps);
+		    }
+	    else
+		console.log(status);
+		});
+	
+		
+	    });
+	}
+    }
+
+    function loadSteps(steps){
+	var tb = document.getElementById("directions");
+	for(var i = 0;i<steps.length;i++){
+	    var r = tb.insertRow(tb.rows.length); //adds to end
+	    var c1 = r.insertCell(0);
+	    c1.innerHTML = steps[i].instructions;
+	}
+    }
+    
+    eButton.addEventListener("click",calcETA);
     initialize();
 }		      
 
