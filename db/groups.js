d71cd501cc09a1b70b4fe41f292d46f7545f8745
Flygoniq Jiang <flystlt@gmail.com>
2015-06-23 18:05:15 -0400

    Fixed getinfo for Groups

--- db/groups.js
+++ db/groups.js
@@ -16,7 +16,28 @@ exports.removeUserFromGroup = function(userid,groupid,callback){
     mysql.remove('user_groups','userid = ' + userid + ' AND groupid = ' + groupid,callback);
 }
 
-//Get info from a group by calling its name.  Can be changed to call by id.
-exports.getInfoFromGroup = function(group_name,callback){
-    mysql.get('id,group_name,leader_id','groups','group_name = "' + group_name + '"',callback);
+/* Get info from a group by calling its id.  Can be changed to call by name.
+ * Inputs: int id, which is the group's id
+ *         callback function with 2 parameters, error and result
+ * Returns {id,group_name,leader_id,{list of users and info in the order user_id,
+ *          username,email,name}}
+ */
+exports.getInfoFromGroup = function(id,callback){
+    mysql.get('*','groups','id = "' + id + '"',function(err,results){
+        var result = results[0];
+        getGroupsUsers(id,function(err,results){
+            result.users = results;
+            callback(err,result);
+        });
+    });
+}
+
+//Returns the user infos of all the users in a group
+var getGroupsUsers = function(id,callback){
+    mysql.get('user_groups.user_id,users.username,users.email,users.name',
+	      'user_groups LEFT JOIN users ON user_groups.user_id = users.id',
+	      'group_id = ' + id,
+	      function(err,results){
+            callback(err,results);
+	      });
 }
