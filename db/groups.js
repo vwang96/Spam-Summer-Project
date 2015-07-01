ca29619a9523067f9b6a555b140be7e73817d502
Flygoniq Jiang <flystlt@gmail.com>
2015-07-01 17:31:52 -0400

    Added comments describing functionality?

--- db/groups.js
+++ db/groups.js
@@ -1,17 +1,45 @@
 var mysql = require('./mysql');
 
+/* Find and return users by id and username
+ *
+ * @param name Name of user
+ * @param callback a function performed at the end of get.
+ *   Receives the results of get.
+ */
 exports.findUsers = function(name,callback){
     mysql.get('username,id','users','username LIKE "' + name + '%"',callback);
 }
 
+/* Creates a group
+ *
+ * @param userid ID of creator.
+ * @param groupName Name of group.
+ * @param callback a function performed at the end of insert.
+ *   Receives the results of insert.
+ */
 exports.createGroup = function(userid,groupName,callback){
     mysql.insert('','groups','"'+ groupName + '",' + userid,callback);
 }
+
+/* Add a specified user to a group.
+ *
+ * @param userid ID of user.
+ * @param groupid ID of group.
+ * @param callback a function performed at the end of insert.
+ *   Receives the results of insert.
+ */
 exports.addUserToGroup = function(userid,groupid,callback){
     mysql.insert('','user_groups',userid + ',' + groupid,callback);
-
 }
 
+/* Remove a specified user from a group.
+ *
+ * @param userid ID of user.
+ * @param groupid ID of group.
+ * @param callback a function performed at the end of remove.
+ *   Receives the results of remove.
+ * @result user that matches userid and groupid
+ */
 exports.removeUserFromGroup = function(userid,groupid,callback){
     mysql.remove('user_groups','userid = ' + userid + ' AND groupid = ' + groupid,callback);
 }
@@ -19,7 +47,7 @@ exports.removeUserFromGroup = function(userid,groupid,callback){
 /* Get info from a group by calling its id.  Can be changed to call by name.
  * Inputs: int id, which is the group's id
  *         callback function with 2 parameters, error and result
- * Returns {id,group_name,leader_id,{list of users and info in the order user_id,
+ * Result: {id,group_name,leader_id,{list of users and info in the order user_id,
  *          username,email,name}}
  */
 exports.getInfoFromGroup = function(id,callback){
